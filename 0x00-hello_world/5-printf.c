31cd506eb3757316d40fe253f7d766c248e2ffa5
mohamedennaji37 <1337mohamed1@gmail.com>
2023-06-15 06:35:52 -0700

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0);
+}
