60d5c5925bd3c12c9bfe2832d2d2cfc4dec18dd6
mohamedennaji37 <1337mohamed1@gmail.com>
2023-06-24 19:29:00 -0700

    l

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,16 @@
  */
 
 int main(void)
+
 {
-char c[9] = "_putchar";
-int i = 0;
-while (i < 8)
-{
-_putchar(c[i]);
-i++;
-}
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
 return (0);
 }
