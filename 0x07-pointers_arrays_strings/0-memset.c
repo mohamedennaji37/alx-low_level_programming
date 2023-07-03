c5b80e810eba17a68394a37f4e297bb58ea1eb90
mohamedennaji37 <1337mohamed1@gmail.com>
2023-07-03 15:00:04 -0700

    l

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int ate;
+	for(ate = 0 ; n > 0 ; ate++ , n--)
+	{
+		s[ate] = b ;
+	}
+	
+	return(s);
+	
+
+
+}
+
+
